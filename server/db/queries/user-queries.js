dfca629d573cf6b5549a23cafb74824b42301400
Grace <wangxiaotian2012@gmail.com>
2022-05-15 14:04:33 -0700

    Setup homepage router. Add cookie parser.

--- server/db/queries/user-queries.js
+++ server/db/queries/user-queries.js
@@ -1,7 +1,18 @@
 const db = require("../db-connection");
 
 const getUserById = (id) => {
-
+  return db.query(`
+  SELECT *
+  FROM users
+  WHERE id = ${id};
+  `)
+    .then((result) => {
+      const user = result.rows[0];
+      return user;
+    })
+    .catch((err) => {
+      console.log("getUserById error: ", err.message);
+    });
 };
 
 module.exports = { getUserById };
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 // Web server config
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -12,24 +13,23 @@ const app = express();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(express.json());
 
 // Separated Routes for each Resource
 const usersRouter = require("./routes/users");
-
+const homepageRouter = require("./routes/homepage");
 
 // Mount all resource routes
 app.use("/users", usersRouter);
-
+app.use("/", homepageRouter);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get("/", (req, res) => {
-  res.send("homepage");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
