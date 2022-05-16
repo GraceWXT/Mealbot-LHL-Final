8d120847071d5204cd8277002710f403318dee64
Grace <wangxiaotian2012@gmail.com>
2022-05-16 11:09:06 -0700

    Fix ERD. Remove api username and hash from db. Modify routes accordingly

--- server/db/schema/01_users_schema.sql
+++ server/db/schema/01_users_schema.sql
@@ -6,8 +6,6 @@ CREATE TABLE users (
   last_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   img_url text,
-  api_user_hash VARCHAR(255) NOT NULL,
-  api_username VARCHAR(255) NOT NULL,
   max_calories INTEGER,
   max_time INTEGER,
   servings INTEGER
