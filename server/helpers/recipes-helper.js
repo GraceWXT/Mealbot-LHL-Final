867c9b3c5503e22485de57c7c7b00b84c3193c1d
Lucas Tan <sheng.x.tan@gmail.com>
2022-05-16 23:03:38 +0000

    commit recipe helper

--- server/helpers/recipes-helper.js
+++ server/helpers/recipes-helper.js
@@ -3,6 +3,7 @@
 const getRecipeInfo = (recipe) => {
   let cleanedRecipe = {};
 
+  //get ingredients information
   const getIngredients = (recipe) => {
     let ingredients = [];
     recipe.extendedIngredients.map(ingredient =>  {
@@ -15,6 +16,19 @@ const getRecipeInfo = (recipe) => {
     return ingredients;
   };
 
+  const getInstructions = (recipe) => {
+    let instructions = [];
+    recipe.analyzedInstructions[0].steps.map( instruction => {
+      instructions.push({
+        number: instruction.number,
+        step: instruction.step
+      })
+    })
+    return instructions;
+  }
+
+
+  //get nutrition information
   const getNutrition = (recipe) => {
     let nutritionalInfo = [];
     const nutrition = recipe.nutrition.nutrients
@@ -33,7 +47,7 @@ const getRecipeInfo = (recipe) => {
   }
 
   cleanedRecipe["ingredients"] = getIngredients(recipe);
-  cleanedRecipe["instructions"] = recipe.instructions;
+  cleanedRecipe["instructions"] = getInstructions(recipe);
   cleanedRecipe["nutrition"] = getNutrition(recipe);
 
   return cleanedRecipe;
