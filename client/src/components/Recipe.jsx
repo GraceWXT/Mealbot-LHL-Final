6deb369d33b8ef456fa6baef49336e29e564e7bc
Lucas Tan <sheng.x.tan@gmail.com>
2022-05-16 19:12:12 -0600

    add useEffect and useState to grab data from backend

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -1,4 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+// import classNames from "classnames";
+
+
 export default function Recipe() {
+  const [state, setState] = useState({
+    ingredients: [],
+    instructions: [],
+    nutrition: []
+  });
+
+
+  const { id } = useParams();
+  console.log('id', id);
+
+  // get data to update state
+  useEffect(() => {
+    axios.get(`/recipes/${id}`
+    ).then((res) => {
+      // console.log("res", res.data);
+      setState(prev => ({ ...prev, ...res.data }));
+    });
+  }, []);
+  console.log('state', state);
+
   return (
     <div>
       Recipe
