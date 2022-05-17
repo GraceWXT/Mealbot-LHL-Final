7676a07543a9fb9c70ef9395a408b0c382129fc3
Lucas Tan <sheng.x.tan@gmail.com>
2022-05-17 14:45:15 -0600

    add components and state values to recipe page

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -9,12 +9,18 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import { Stack, HStack, VStack } from '@chakra-ui/react';
 import { Container } from '@chakra-ui/react';
 import { Divider } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 
 export default function Recipe() {
   const [state, setState] = useState({
     ingredients: [],
     instructions: [],
-    nutrition: []
+    nutrition: [],
+    title: "",
+    readyInMinutes: "",
+    image: ""
   });
 
 
@@ -33,13 +39,15 @@ export default function Recipe() {
 
   return (
     <HStack>
-      <Container w="40%" >
-      something
+      <Container w="40%" py={20}>
+        <Heading as='h3' size='md'>{state.title}</Heading>
+        <Text>Ready in {state.readyInMinutes}min</Text>
+        <Image src={state.image} py={5}/>
       </Container>
 
       <Divider orientation='vertical'/>
 
-      <Tabs isFitted variant='enclosed' w="60%">
+      <Tabs isFitted variant='enclosed' w="60%" align="top">
         <TabList mb='1em'>
           <Tab>One</Tab>
           <Tab>Two</Tab>
