c389338a602a925e29084c6c0e8ca956d21f7693
Grace <wangxiaotian2012@gmail.com>
2022-05-18 23:05:50 -0700

    Fix meal plan sorter logic

--- client/src/helpers/mealplan-helper.js
+++ client/src/helpers/mealplan-helper.js
@@ -1,6 +1,6 @@
 const mealPlanSorter = (mealplan, slot) => {
   return mealplan.filter(meal => meal.slot === slot)
-    .sort((meal1, meal2) => meal1.date < meal2.date);
+    .sort((meal1, meal2) => meal1.date - meal2.date);
 };
 
 const headerTextHelper = (slot) => {
