0b8f993c427f1957909cbe660270fd3a0800d8a0
Grace <wangxiaotian2012@gmail.com>
2022-05-18 23:04:13 -0700

    Add setStartDate logic to home page button

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,16 +1,18 @@
-import {
-  Button
-} from "@chakra-ui/react";
+import {  Button } from "@chakra-ui/react";
 import { Link, useOutletContext } from "react-router-dom";
+// Helper functions
+import { getNextMondayDate } from "helpers/date-helper";
 
 export default function HomePage() {
-  const { nextMondayDate } = useOutletContext();
+  const { setStartDate } = useOutletContext();
+  //Set Initial start date to next Monday
+  const nextMondayDate = getNextMondayDate();
 
   return (
     <div>
       HomePage
       <Link to={`mealplan/${nextMondayDate}`}>
-        <Button>
+        <Button onClick={() => setStartDate(nextMondayDate)}>
           I'm feeling Hungry!
         </Button>
       </Link>
