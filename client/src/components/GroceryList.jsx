9eb941f74424995d4b2f8dc8d4dfee1d9f8f693a
Lucas Tan <sheng.x.tan@gmail.com>
2022-05-19 22:34:35 -0600

    filtered aisles for aisle names, ingredient lists, and twilio text

--- client/src/components/GroceryList.jsx
+++ client/src/components/GroceryList.jsx
@@ -44,17 +44,24 @@ export default function GroceryList() {
 
   const { aisles } = groceryList;
 
+  //FILTER OUT
+  const filter = ["Oil, Vinegar, Salad Dressing", "Spices and Seasonings", "Condiments", "Pantry Items", "Sweet Snacks", "Dried Fruits", "Ethnic Foods", "Generic", "Savory Snacks", "Nut butters, Jams, and Honey", "Alcoholic Beverages"];
+
+
   // AISLE NAMES
   const linkofAisleNames = aisles.map(aisle => {
 
     const linkText = aisle.aisle.replaceAll(" ", "-");
-    return (
-      <ListItem key={aisle.aisle} py={0.5} borderBottom='1px' borderColor='gray.200'>
-        <Link href={`#${linkText}`}>
-          {aisle.aisle}
-        </Link>
-      </ListItem>
-    );
+    if (!filter.includes(aisle.aisle)) {
+      return (
+        <ListItem key={aisle.aisle} py={0.5} borderBottom='1px' borderColor='gray.200'>
+          <Link href={`#${linkText}`}>
+            {aisle.aisle}
+          </Link>
+        </ListItem>
+      );
+    }
+
   });
 
   // Twilio Button
@@ -73,40 +80,44 @@ export default function GroceryList() {
   // ITEMS QUANTITY
 
 
+
   // GROCERY LIST
   const listOfAisleItems = aisles.map((aisle) => {
 
-    // AISLE ITEMS
-    const aisleItems = aisle.items.map(item => {
-      const measurement = item.measures.original;
-      const { amount, unit } = measurement;
-      const quantity = (`${Number(amount.toFixed(2))} ${unit}`);
-      // console.log(quantity);
+    if (!filter.includes(aisle.aisle)) {
+      // AISLE ITEMS
+      const aisleItems = aisle.items.map(item => {
+        const measurement = item.measures.original;
+        const { amount, unit } = measurement;
+        const quantity = (`${Number(amount.toFixed(2))} ${unit}`);
+        // console.log(quantity);
+        return (
+          <ListItem key={item.id} py={2} borderBottom='1px' borderColor='gray.200'>
+            <Checkbox colorScheme="green" spacing="1rem" width="100%">
+              {quantity} {item.name}
+            </Checkbox>
+          </ListItem>
+        );
+      });
+
+      const linkText = aisle.aisle.replaceAll(" ", "-");
+
       return (
-        <ListItem key={item.id} py={2} borderBottom='1px' borderColor='gray.200'>
-          <Checkbox colorScheme="green" spacing="1rem" width="100%">
-            {quantity} {item.name}
-          </Checkbox>
-        </ListItem>
+        <VStack
+          key={`${aisle.aisle} - ${aisleItems.length}`}
+          margin="1em"
+          alignItems="start" >
+          {/* AISLE LINKS TO NAV TO AISLE NAME*/}
+          <Heading fontSize="large" id={`${linkText}`}>
+            {aisle.aisle}
+          </Heading>
+          <List textAlign="start" >
+            {aisleItems}
+          </List>
+        </VStack>
       );
-    });
+    }
 
-    const linkText = aisle.aisle.replaceAll(" ", "-");
-
-    return (
-      <VStack
-        key={`${aisle.aisle} - ${aisleItems.length}`}
-        margin="1em"
-        alignItems="start" >
-        {/* AISLE LINKS TO NAV TO AISLE NAME*/}
-        <Heading fontSize="large" id={`${linkText}`}>
-          {aisle.aisle}
-        </Heading>
-        <List textAlign="start" >
-          {aisleItems}
-        </List>
-      </VStack>
-    );
   });
 
   return (
@@ -117,7 +128,7 @@ export default function GroceryList() {
           {/* AISLES */}
           <VStack height="100%" padding="20px">
             <Heading fontSize="1.8rem" >Aisles</Heading>
-            <List padding="1em" >
+            <List padding="1em">
               {linkofAisleNames}
             </List>
           </VStack>
@@ -130,12 +141,12 @@ export default function GroceryList() {
                 icon={<CopyIcon />}
                 colorScheme={useColorModeValue("turquoiseGreen", "majestyPurple")}
                 borderRadius="50%"
-                size=""
+                size="sm"
               />
               <Tooltip label='Text the grocery list to your saved phone number'>
                 <IconButton
                   onClick={sendTwilio}
-                  icon={<MdOutlineTextsms boxSize={20}/>}
+                  icon={<MdOutlineTextsms boxSize={20} />}
                   colorScheme={useColorModeValue("turquoiseGreen", "majestyPurple")}
                   borderRadius="50%"
                   size="sm" />
