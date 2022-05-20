3eee353eb22b7cb923984eb9c933e6bde5e93b59
Grace <wangxiaotian2012@gmail.com>
2022-05-20 14:14:26 -0700

    Change client side testing meal plan to match server

--- client/src/components/MealPlan/testing-mealplan.js
+++ client/src/components/MealPlan/testing-mealplan.js
@@ -1,234 +1,18 @@
 const testingMealPlan = [
   {
-    "date": 1589785200,
+    "date": 1653807600,
     "slot": 1,
     "position": 0,
     "type": "RECIPE",
     "value": {
-      "id": 632928,
+      "id": 1697551,
       "servings": 2,
-      "title": "Asparagus Eggs Benedict",
-      "image": "https://spoonacular.com/recipeImages/632928-312x231.jpg"
+      "title": "Good Old Fashioned Pancakes",
+      "image": "https://spoonacular.com/recipeImages/1697551-312x231.jpg"
     }
   },
   {
-    "date": 1589785200,
-    "slot": 2,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 1697553,
-      "servings": 2,
-      "title": "Pan-Seared Honey Glazed Salmon with Collard Greens",
-      "image": "https://spoonacular.com/recipeImages/1697553-312x231.jpg"
-    }
-  },
-  {
-    "date": 1589785200,
-    "slot": 3,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 715523,
-      "servings": 2,
-      "title": "Chorizo and Beef Quinoa Stuffed Pepper",
-      "image": "https://spoonacular.com/recipeImages/715523-312x231.jpg"
-    }
-  },
-  {
-    "date": 1589871600,
-    "slot": 1,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 657306,
-      "servings": 2,
-      "title": "Pumpkin French Toast",
-      "image": "https://spoonacular.com/recipeImages/657306-312x231.jpg"
-    }
-  },
-  {
-    "date": 1589871600,
-    "slot": 2,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 715383,
-      "servings": 2,
-      "title": "Slow Cooker Chicken and Dumplings",
-      "image": "https://spoonacular.com/recipeImages/715383-312x231.jpg"
-    }
-  },
-  {
-    "date": 1589871600,
-    "slot": 3,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 716408,
-      "servings": 2,
-      "title": "Greek-Style Baked Fish: Fresh, Simple, and Delicious",
-      "image": "https://spoonacular.com/recipeImages/716408-312x231.jpg"
-    }
-  },
-  {
-    "date": 1589958000,
-    "slot": 1,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 1098387,
-      "servings": 2,
-      "title": "Quinoa Salad with Barberries & Nuts",
-      "image": "https://spoonacular.com/recipeImages/1098387-312x231.jpg"
-    }
-  },
-  {
-    "date": 1589958000,
-    "slot": 2,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 637264,
-      "servings": 2,
-      "title": "Cashew Butter Chicken",
-      "image": "https://spoonacular.com/recipeImages/637264-312x231.jpg"
-    }
-  },
-  {
-    "date": 1589958000,
-    "slot": 3,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 1000566,
-      "servings": 2,
-      "title": "Easy Instant Pot Beef Tips and Rice",
-      "image": "https://spoonacular.com/recipeImages/1000566-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590044400,
-    "slot": 1,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 646515,
-      "servings": 2,
-      "title": "Healthy Southwestern Oatmeal",
-      "image": "https://spoonacular.com/recipeImages/646515-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590044400,
-    "slot": 2,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 715769,
-      "servings": 2,
-      "title": "Broccolini Quinoa Pilaf",
-      "image": "https://spoonacular.com/recipeImages/715769-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590044400,
-    "slot": 3,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 632874,
-      "servings": 2,
-      "title": "Asian Salmon Burgers With Tangy Ginger Lime Sauce",
-      "image": "https://spoonacular.com/recipeImages/632874-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590130800,
-    "slot": 1,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 796873,
-      "servings": 2,
-      "title": "Yogurt Parfait",
-      "image": "https://spoonacular.com/recipeImages/796873-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590130800,
-    "slot": 2,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 982365,
-      "servings": 2,
-      "title": "Instant Pot Hawaiian Chicken",
-      "image": "https://spoonacular.com/recipeImages/982365-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590130800,
-    "slot": 3,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 1070648,
-      "servings": 2,
-      "title": "Easy Tomato Basil Chicken – One Pot Meal",
-      "image": "https://spoonacular.com/recipeImages/1070648-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590217200,
-    "slot": 1,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 1082038,
-      "servings": 2,
-      "title": "Big Batch Smoothie Bowl",
-      "image": "https://spoonacular.com/recipeImages/1082038-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590217200,
-    "slot": 2,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 652417,
-      "servings": 2,
-      "title": "Moroccan chickpea and lentil stew",
-      "image": "https://spoonacular.com/recipeImages/652417-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590217200,
-    "slot": 3,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 642585,
-      "servings": 2,
-      "title": "Farfalle with fresh tomatoes, basil and mozzarella",
-      "image": "https://spoonacular.com/recipeImages/642585-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590303600,
-    "slot": 1,
-    "position": 0,
-    "type": "RECIPE",
-    "value": {
-      "id": 643674,
-      "servings": 2,
-      "title": "Fried Brown Rice",
-      "image": "https://spoonacular.com/recipeImages/643674-312x231.jpg"
-    }
-  },
-  {
-    "date": 1590303600,
+    "date": 1653807600,
     "slot": 2,
     "position": 0,
     "type": "RECIPE",
@@ -240,7 +24,7 @@ const testingMealPlan = [
     }
   },
   {
-    "date": 1590303600,
+    "date": 1653807600,
     "slot": 3,
     "position": 0,
     "type": "RECIPE",
@@ -249,6 +33,222 @@ const testingMealPlan = [
       "servings": 2,
       "title": "Chicken Ranch Burgers",
       "image": "https://spoonacular.com/recipeImages/991010-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653289200,
+    "slot": 1,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 632928,
+      "servings": 2,
+      "title": "Asparagus Eggs Benedict",
+      "image": "https://spoonacular.com/recipeImages/632928-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653289200,
+    "slot": 2,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 664975,
+      "servings": 2,
+      "title": "Wasabi & Honey Glazed-Salmon",
+      "image": "https://spoonacular.com/recipeImages/664975-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653289200,
+    "slot": 3,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 715523,
+      "servings": 2,
+      "title": "Chorizo and Beef Quinoa Stuffed Pepper",
+      "image": "https://spoonacular.com/recipeImages/715523-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653375600,
+    "slot": 1,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 657306,
+      "servings": 2,
+      "title": "Pumpkin French Toast",
+      "image": "https://spoonacular.com/recipeImages/657306-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653375600,
+    "slot": 2,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 715383,
+      "servings": 2,
+      "title": "Slow Cooker Chicken and Dumplings",
+      "image": "https://spoonacular.com/recipeImages/715383-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653375600,
+    "slot": 3,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 641408,
+      "servings": 2,
+      "title": "Delicious Sausage & Peppers",
+      "image": "https://spoonacular.com/recipeImages/641408-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653462000,
+    "slot": 1,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 1697687,
+      "servings": 2,
+      "title": "Spinach Mushroom Omelette with Parmesan",
+      "image": "https://spoonacular.com/recipeImages/1697687-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653462000,
+    "slot": 2,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 637264,
+      "servings": 2,
+      "title": "Cashew Butter Chicken",
+      "image": "https://spoonacular.com/recipeImages/637264-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653462000,
+    "slot": 3,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 1000566,
+      "servings": 2,
+      "title": "Easy Instant Pot Beef Tips and Rice",
+      "image": "https://spoonacular.com/recipeImages/1000566-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653548400,
+    "slot": 1,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 646515,
+      "servings": 2,
+      "title": "Healthy Southwestern Oatmeal",
+      "image": "https://spoonacular.com/recipeImages/646515-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653548400,
+    "slot": 2,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 715769,
+      "servings": 2,
+      "title": "Broccolini Quinoa Pilaf",
+      "image": "https://spoonacular.com/recipeImages/715769-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653548400,
+    "slot": 3,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 656746,
+      "servings": 2,
+      "title": "Pork chops with cider and spinach",
+      "image": "https://spoonacular.com/recipeImages/656746-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653634800,
+    "slot": 1,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 796873,
+      "servings": 2,
+      "title": "Yogurt Parfait",
+      "image": "https://spoonacular.com/recipeImages/796873-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653634800,
+    "slot": 2,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 982365,
+      "servings": 2,
+      "title": "Instant Pot Hawaiian Chicken",
+      "image": "https://spoonacular.com/recipeImages/982365-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653634800,
+    "slot": 3,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 1070648,
+      "servings": 2,
+      "title": "Easy Tomato Basil Chicken – One Pot Meal",
+      "image": "https://spoonacular.com/recipeImages/1070648-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653721200,
+    "slot": 1,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 1082038,
+      "servings": 2,
+      "title": "Big Batch Smoothie Bowl",
+      "image": "https://spoonacular.com/recipeImages/1082038-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653721200,
+    "slot": 2,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 652417,
+      "servings": 2,
+      "title": "Moroccan chickpea and lentil stew",
+      "image": "https://spoonacular.com/recipeImages/652417-312x231.jpg"
+    }
+  },
+  {
+    "date": 1653721200,
+    "slot": 3,
+    "position": 0,
+    "type": "RECIPE",
+    "value": {
+      "id": 641565,
+      "servings": 2,
+      "title": "Donkatsu - Korean Breaded Pork Cutlet",
+      "image": "https://spoonacular.com/recipeImages/641565-312x231.jpg"
     }
   }
 ];
