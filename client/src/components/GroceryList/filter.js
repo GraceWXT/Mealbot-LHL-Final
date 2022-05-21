dfc865b8eed1f9f1ff8168765731177b95330568
Grace <wangxiaotian2012@gmail.com>
2022-05-21 16:07:40 -0700

    Move filter array to a separate file

---
+++ client/src/components/GroceryList/filter.js
@@ -0,0 +1,15 @@
+const filter = [
+  "Oil, Vinegar, Salad Dressing",
+  "Spices and Seasonings",
+  "Condiments",
+  "Pantry Items",
+  "Sweet Snacks",
+  "Dried Fruits",
+  "Ethnic Foods",
+  "Generic",
+  "Savory Snacks",
+  "Nut butters, Jams, and Honey",
+  "Alcoholic Beverages"
+];
+
+export default filter;
