81d171906cc8d42c0998e985f64143546f3e1250
Grace <wangxiaotian2012@gmail.com>
2022-05-21 13:40:03 -0700

    Add draggingMeal State to MealPlanTable. Move meal.value logic to Meal.

--- client/src/components/MealPlan/MealPlanTable.jsx
+++ client/src/components/MealPlan/MealPlanTable.jsx
@@ -1,10 +1,10 @@
 // External components and hooks
+import { useState } from "react";
 import {
   Table,
   Thead,
   Tbody,
   Tr,
-  Td,
   Th,
 } from "@chakra-ui/react";
 
@@ -17,6 +17,8 @@ import { mealPlanSorter, headerTextHelper } from "helpers/mealplan-helper";
 
 export default function MealPlanTable(props) {
   const { mealPlan } = props;
+  const [draggingMeal, setDraggingMeal] = useState();
+
   // Restructure the mealPlan state into array of 3 arrays
   const breakfasts = mealPlanSorter(mealPlan, 1) || [];
   const lunches = mealPlanSorter(mealPlan, 2) || [];
@@ -30,20 +32,15 @@ export default function MealPlanTable(props) {
     const headerText = headerTextHelper(index + 1);
 
     let tds = [];
-    if (!row.length) {
-      // When the row is empty, push 7 empty Tds to the row
-      for (let i = 0; i <= 6; i++) {
-        tds.push(<Td key={`${index}-${i}`} py="8px" height="20vh"></Td>);
-      }
-
-    } else {
-      // When the row is not empty
-      // Check if there is a recipe value for each meal and render the meal or an empty Td
-      for (let i = 0; i < row.length; i++) {
-        row[i].value ?
-          tds.push(<Meal key={row[i].value.id} meal={row[i]} />) :
-          tds.push(<Td key={`${index}-${i}`} py="8px" height="20vh"></Td>);
-      }
+    // When the row is not empty
+    // Check if there is a recipe value for each meal and render the meal or an empty Td
+    for (let i = 0; i < row.length; i++) {
+      tds.push(
+        <Meal key={`${index}-${i}`}
+          meal={row[i]}
+          draggingMeal={draggingMeal}
+          setDraggingMeal={setDraggingMeal}
+        />);
     }
 
     // Return the table row with a header and 7 Tds
