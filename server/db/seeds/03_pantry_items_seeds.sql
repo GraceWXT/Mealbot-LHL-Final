0a455f52e90e4f7999c59c8dc3ea63fe22148308
Lucas Tan <sheng.x.tan@gmail.com>
2022-05-23 08:15:34 +0000

    add items into pantry list seeds

--- server/db/seeds/03_pantry_items_seeds.sql
+++ server/db/seeds/03_pantry_items_seeds.sql
@@ -1,2 +1,13 @@
 INSERT INTO pantry_items (user_id, name)
 VALUES (1, 'salt')
+(1, 'oil'),
+(1, 'cider vinegar'),
+(1, 'black pepper'),
+(1, 'jam'),
+(1, 'ketchup'),
+(1, 'mustard'),
+(1, 'mayonnaise'),
+(1, 'salad dressing'),
+(1, 'honey'),
+(1, 'garlic powder'),
+(1, 'shortening')
