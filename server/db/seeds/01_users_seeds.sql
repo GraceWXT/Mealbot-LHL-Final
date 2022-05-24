eac39ffb4dcf83c625737d69969b9f5abc85d08c
Grace <wangxiaotian2012@gmail.com>
2022-05-24 15:57:59 -0700

    Update user email

--- server/db/seeds/01_users_seeds.sql
+++ server/db/seeds/01_users_seeds.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (first_name, last_name, email, img_url,max_calories, max_time, servings)
-VALUES ('Gordon', 'Ramsay', 'idiotsandwich@email.com', 'https://i.imgur.com/dsTjcWuh.jpg', 800, 30, 2)
+VALUES ('Gordon', 'Ramsay', 'lambsauce@email.com', 'https://i.imgur.com/dsTjcWuh.jpg', 800, 30, 2)
