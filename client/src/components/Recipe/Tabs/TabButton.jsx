de094e5431acccc02014aa5827e75a119f93e104
Grace <wangxiaotian2012@gmail.com>
2022-05-24 21:33:11 -0700

    Fix tab button bg color setting

--- client/src/components/Recipe/Tabs/TabButton.jsx
+++ client/src/components/Recipe/Tabs/TabButton.jsx
@@ -1,13 +1,12 @@
 import {
   Tab, useColorModeValue
 } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
 export default function TabButton(props) {
   return (
     <Tab
       border="1px"
       borderColor={useColorModeValue("gray.200", "gray.700")}
-      bg={useColorModeValue(mode("hsl(0 0% 93%)", "gray.800"), "gray.900")}
+      bg={useColorModeValue("hsl(0 0% 92%)", "gray.600")}
       _selected={{ bg: useColorModeValue("turquoiseGreen.100", "majestyPurple.500") }}
       _hover={{ bg: useColorModeValue("turquoiseGreen.300", "majestyPurple.600") }}
       _active={{ bg: useColorModeValue("turquoiseGreen.500", "majestyPurple.700") }}
